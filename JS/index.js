a5cececcddac19a4dfa069906e539f94d05d991b
Antonio Piras <antonio.piras90@gmail.com>
2023-07-07 17:33:37 +0200

    esercizio con ciclo for

--- JS/index.js
+++ JS/index.js
@@ -13,47 +13,49 @@ let arrayNum = [];
 let sum = 0
 
 
-// creo costante per bottone genera 
+// // creo costante per bottone genera 
 
-const genera = document.querySelector(".genera")
+// const genera = document.querySelector(".genera")
 
-// creo evento al click del bottone genera 
+// // creo evento al click del bottone genera 
 
-genera.addEventListener("click", function () {
+// genera.addEventListener("click", function () {
 
-     // chiedo i numeri all'utente tramite prompt
-const  inputUtente = parseInt(prompt("inserisci 1 numeri "));
+//      // chiedo i numeri all'utente tramite prompt
+// const  inputUtente = parseInt(prompt("inserisci 1 numeri "));
 
-// creo condizione se somma è inferiore a 50 popolo array 
+// // creo condizione se somma è inferiore a 50 popolo array 
 
-  if (sum < 50) {
+//   if (sum < 50) {
 
-     // do un nuovo valore alla variablie di somma 
+//      // do un nuovo valore alla variablie di somma 
 
-     sum += inputUtente;
-     // log della somma 
-     console.log(sum);
-     // poopolo l'array
-     arrayNum.push(inputUtente)
-     // log dell' array popolato da prompt utente 
-     console.log(arrayNum);
-  }else {
-     // stampo alert su pagina per comunicare all'utente di aver raggiunto la somma di 50
-     alert("hai raggiunto la somma");
-  }
-});
-
-// for (let i = 0;  sum < 50; i++) {
-//      const  inputUtente = parseInt(prompt("inserisci 1 numeri "));
-    
-//     if (sum < 50) {
 //      sum += inputUtente;
+//      // log della somma 
 //      console.log(sum);
-//      arrayNum.push(inputUtente);
-    
+//      // poopolo l'array
+//      arrayNum.push(inputUtente)
+//      // log dell' array popolato da prompt utente 
+//      console.log(arrayNum);
+//   }else {
+//      // stampo alert su pagina per comunicare all'utente di aver raggiunto la somma di 50
+//      alert("hai raggiunto la somma");
 //   }
+// });
+// creo ciclo per chiedere ad utente numeri fincheè la somma dei numeri non raggiunge 50 
 
-// };
-// alert("hai raggiunto la somma");
+for (let i = 0;  sum < 50; i++) {
+       // chiedo i numeri all'utente tramite prompt
+     const  inputUtente = parseInt(prompt("inserisci 1 numeri "));
+    // // creo condizione se somma è inferiore a 50 popolo array
+    if (sum < 50) {
+     sum += inputUtente;
+     console.log(sum);
+     arrayNum.push(inputUtente);
+    
+  }
+
+};
+alert("hai raggiunto la somma");
 
 
