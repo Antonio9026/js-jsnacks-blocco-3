3a9a9dbd596b91e649b739c793a2f90115ec0ed2
Antonio Piras <antonio.piras90@gmail.com>
2023-07-07 19:22:32 +0200

    Esercizio 2 corretto

--- Snack 2/script.js
+++ Snack 2/script.js
@@ -2,37 +2,31 @@
 // Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l’altro di verde. Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 
 
+// creo array di numeri
 
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numbers = [2, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,33,124,432,567,876,957,117,737];
 
-let pair = 0
+
+// seleziono elementi HTML 
 const red = document.getElementById("red")
 const green = document.getElementById("green")
-let unpair = 0
+
+
 for (let i = 0; i < numbers.length; i++) {
    
     // creo condizione per selezionare elementi in posizione dispari
-    if (i % 2 !== 0) {
+    if (numbers[i] % 2 === 0) {
         const pairElements = document.createElement("div");
         pairElements.classList.add("pair")
-       
-         pair = numbers[i];
-
-        // log della posizione elementi 
-        console.log([i]);
         red.append(pairElements)
-        pairElements.innerHTML += pair
+        pairElements.innerHTML += numbers[i]
 
     
         
     }else{
         const unpairElements = document.createElement("div");
         unpairElements.classList.add("unpair")
-       unpair = numbers[i];
-        // log della posizione elementi 
-        console.log([i]);
-        console.log([i]);
         green.append(unpairElements)
-        unpairElements.innerHTML += unpair
+        unpairElements.innerHTML += numbers[i]
     }
 };
